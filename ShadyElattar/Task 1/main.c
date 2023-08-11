994ad1ad5f2bd6a293cc8638f8b9d84b455e1d31
shadyalattarr <141693614+shadyalattarr@users.noreply.github.com>
2023-08-11 21:11:03 +0300

    Add files via upload

--- ShadyElattar/Task 1/main.c
+++ ShadyElattar/Task 1/main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+int max_of_four(int a, int b, int c, int d)
+{
+    return max(a,max(b,max(c,d)));
+}
+
+int max(int x, int y)
+{
+    if(x>y)
+        return x;
+    return y;
+}
 
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i = n-1;i>=0;i--)
-        scanf("%d",&arr[i]);
-    for(int i = 0;i<n;i++)
-        printf("%d ",arr[i]);
+    int a,b,c,d;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+
+    printf("%d",max_of_four(a,b,c,d));
 
     return 0;
 }
